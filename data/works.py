cc55a1a5ac976c5169826909d66b9f8d4aaa5ba8
DanielAD <anikindaniel2007@gmail.com>
2023-04-24 17:19:09 +0300

    gg

---
+++ data/works.py
@@ -0,0 +1,14 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+
+from .db_session import SqlAlchemyBase
+
+
+class Works(SqlAlchemyBase):
+    __tablename__ = 'works'
+
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True)
+    content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    completes = sqlalchemy.Column(sqlalchemy.String, nullable=True)
